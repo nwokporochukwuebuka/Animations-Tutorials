f9be5a342deb217feacdbf66c7e995fce0cda7a3
codedadi <ebuka@tm30.net>
2023-08-21 08:59:08 +0100

    UPDATE: Continue later

--- ScrollToIndex/src/ScrollViewAnimation.tsx
+++ ScrollToIndex/src/ScrollViewAnimation.tsx
@@ -1,8 +1,9 @@
-import {Animated, StyleSheet, Text, View} from 'react-native';
+import {Animated, View} from 'react-native';
 import React, {useEffect, useRef, useState} from 'react';
-import {ScrollView} from 'react-native';
+// import {ScrollView} from 'react-native';
 
-const ScrollViewAnimation = () => {
+// the side effect of this method is that its still slow
+/* const ScrollViewAnimation = () => {
   const [headerShown, setHeaderShown] = useState<boolean>(false);
 
   const translation = useRef(new Animated.Value(-100)).current;
@@ -16,7 +17,7 @@ const ScrollViewAnimation = () => {
   }, [headerShown]);
   return (
     <>
-      {/* Header here */}
+    
       <Animated.View
         style={{
           position: 'absolute',
@@ -45,8 +46,46 @@ const ScrollViewAnimation = () => {
       </ScrollView>
     </>
   );
+}; */
+const ScrollViewAnimation = () => {
+  const scrolling = useRef(new Animated.Value(0)).current;
+
+  //   const translation = useRef(new Animated.Value(-100)).current;
+  // Instead of doing what's above, we can extrapolate
+  const translation = scrolling.interpolate({
+    inputRange: [100, 130],
+    outputRange: [-100, 0],
+    extrapolate: 'clamp',
+  });
+
+  console.log('I am here');
+
+  return (
+    <>
+      {/* Header here */}
+      <Animated.View
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 80,
+          backgroundColor: 'tomato',
+          transform: [{translateY: translation}],
+        }}
+      />
+      <Animated.ScrollView
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrolling}}}],
+          {useNativeDriver: true},
+        )}
+        // onscroll should be fired every 16 ms
+        // scrollEventThrottle={16}
+        style={{flex: 1}}>
+        <View style={{flex: 1, height: 1000}} />
+      </Animated.ScrollView>
+    </>
+  );
 };
 
 export default ScrollViewAnimation;
-
-const styles = StyleSheet.create({});
